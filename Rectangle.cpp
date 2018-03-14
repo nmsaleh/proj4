37821ea2b777fed4d57ebb9d98e7c42c1944261e
lewisconklin <37351627+lewisconklin@users.noreply.github.com>
2018-03-13 22:59:46 -0400

    Update Rectangle.cpp

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -115,13 +115,8 @@ void Rectangle::read(istream& ins) {
 }
 
 void Rectangle::write(ostream& outs) {
-    if (colorTopLeft == colorTopRight && colorTopLeft == colorBottomRight && colorTopLeft == colorBottomLeft) {
-        outs << start << " " << end << " " << colorTopLeft;
-    } 
-    else {
-        outs << start << " " << end << " " << colorTopLeft << " " << colorTopRight
-             << " " << colorBottomRight << " " << colorBottomLeft;
-    }
+    outs << start << " " << end << " " << colorTopLeft << " " << colorTopRight
+         << " " << colorBottomRight << " " << colorBottomLeft;
 }
 
 // Your code goes above this line.
